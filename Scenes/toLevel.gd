91039a16e57fd90fc398c415430e13038a61c1d5
Luis Subiabre <luis.subiabre@gmail.com>
2025-07-05 11:32:44 -0300

    feat: change scene leve1 to level2

---
+++ Scenes/toLevel.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var next_scene_path: String
+
+func _process(delta: float) -> void:
+	pass
+	
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		change_scene()
+		
+func change_scene():
+	## da error de fisicas -> get_tree().change_scene_to_file(next_scene_path)
+	get_tree().call_deferred("change_scene_to_file", next_scene_path)
